88983e08ee5f0c2e8026808b27de7287e6e61ba5
piero-ac <pacoronado01@gmail.com>
2022-05-10 16:25:38 -0400

    Rename from validate_address to address

---
+++ address.py
@@ -0,0 +1,27 @@
+# Module containg functions for validating addresses
+# and getting coordinates for given address
+import geopy
+import geocoder
+from geopy.geocoders import Nominatim
+
+# Function to check for empty input fields
+def empty_fields(user_info):
+	if "" in user_info or None in user_info:
+		return True
+	
+	return False
+
+# Function to check for a valid address
+# Returns a boolean
+def validate(address):
+	valid = geocoder.google(address, key="AIzaSyAzboXO3ZMju2Txx4owH64X8l6B8PqVRRA")
+	return valid.ok
+
+# Function to get the coordinates for an address
+# Returns the latitude and longitude
+def get_coordinates(address):
+	loc = Nominatim(user_agent="GetLoc")
+	get_loc = loc.geocode(address)
+	
+	return get_loc.latitude, get_loc.longitude
+
